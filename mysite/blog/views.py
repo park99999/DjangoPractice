508913ebd9f7f98935a06a73c8176662eafcb88d
Park99 <sungung1030@gmail.com>
2023-03-16 19:02:06 +0900

    day1

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
 def base(request):
-    return render(request, 'blog/base.html')
+    full_text = request.GET['fulltext']
+    word_list = full_text.split()
+
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_dictionary:
+            # Increase
+            word_dictionary[word] += 1
+        else:
+            # add to the dictionary
+            word_dictionary[word] = 1
+
+    return render(request, 'blog/base.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
