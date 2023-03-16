d22de7b2920ae06e7173b68597e39683b445a764
Park99 <sungung1030@gmail.com>
2023-03-16 18:32:26 +0900

    first commit

---
+++ mysite/blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+def base(request):
+    return render(request, 'blog/base.html')
